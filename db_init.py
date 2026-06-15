bd35c2d8219fc507574a4dbc8bb3c183f23aa657
Dipendra Kumar <dipendrak299@gmail.com>
2026-06-15 17:55:14 +0530

    done

--- db_init.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from extensions import db
-
-with app.app_context():
-    try:
-        db.create_all()
-        print("Database tables created successfully for Neon DB!")
-    except Exception as e:
-        print(f"Error creating tables: {e}")
